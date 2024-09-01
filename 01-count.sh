0ef608444f3df7778dffecf376247bd2f6411480
sujithyerra4 <sujithyerra483@gmail.com>
2024-09-01 11:01:55 +0530

    count

--- 01-count.sh
+++ 01-count.sh
@@ -12,15 +12,9 @@ then
 USAGE
 fi
 
-# while IFS= read -r file
-# do 
-
-# awk '{print}' $file |tr '[:upper:]' '[:lower:]'|wc -m
 
 # Remove special characters, convert to lowercase, replace spaces with newlines, and count words
- awk '{print}' $FILE | tr -c '[:alnum:]' '[\n*]' |tr -s '\n'|tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -rn
+ awk '{print}' $FILE | tr '[\n*]' |tr -s '\n'|tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -rn
 
 
-
-# done <<< $FILE
 
