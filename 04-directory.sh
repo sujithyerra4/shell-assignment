6375066bf7c5fec196e28f9d087cba9a8d8026d2
sujithyerra4 <sujithyerra483@gmail.com>
2024-09-01 20:40:54 +0530

    count

--- 04-directory.sh
+++ 04-directory.sh
@@ -1,61 +1,99 @@
-#!/bin/bash
+ #!/bin/bash
+
+# SOURCE_DIR=$1
+# DESTINATION_DIR=$2
+# DAYS=${3:-14}
+# TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+
+# USAGE(){
+
+# echo -e  "$R USAGE $N: sudo sh $0 <source> <destination> <days(optional)>"
+
+# }
+
+# if [ $# -lt 2 ]
+# then
+# USAGE
+# exit 1
+# fi
+
+# if [ ! -d $SOURCE_DIR ]
+#  then
+#    echo  $SOURCE_DIR doesnt exists
+#  fi
+
+# if [ ! -d $DESTINATION_DIR ]
+# then
+#  echo $DESTINATION_DIR doesnt exists
+# fi
+
+# FILES=$(find $SOURCE_DIR -name "*.txt" -mtime "$DAYS")
+# echo Files:$FILES
+
+# if [ ! -z "$FILES" ]  
+#     then
+#     echo files are found
+#     ZIP_FILE=$DESTINATION_DIR/app-logs-$TIME_STAMP.zip
+#     find $SOURCE_DIR -name "*.txt" -mtime "$DAYS"| zip $ZIP_FILE -@
+
+#         if [ -f $ZIP_FILE ]
+#         then 
+
+#         echo succesfully zipped files older than $DAYS
+#         while IFS= read -r file
+#         do
+#         echo "Deleting file:$file"
+#         rm -rf $FILES
+#         done <<< $FILES
+
+#     else
+#         echo -e "Failed  zipping files older than $DAYS"
+#         exit 1
+#         fi
+# else
+#   echo files are not found
+# fi
+
+
+
+# #true only if files are epmty if files are not empty it gives false , then reslut will go to else
+# #instead of this we can also use  -n or -f
+
+
 
 SOURCE_DIR=$1
 DESTINATION_DIR=$2
-DAYS=${3:-14}
-TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
-
-USAGE(){
-
-echo -e  "$R USAGE $N: sudo sh $0 <source> <destination> <days(optional)>"
-
+USAGE() {
+    echo "Usage: $0 <source_directory> <destination_directory>"
 }
 
-if [ $# -lt 2 ]
-then
-USAGE
-exit 1
+# Check for correct number of arguments
+if [ $# -ne 2 ]; then
+    USAGE
+    exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]
- then
-   echo  $SOURCE_DIR doesnt exists
- fi
-
-if [ ! -d $DESTINATION_DIR ]
-then
- echo $DESTINATION_DIR doesnt exists
+# Check if source directory exists
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Source directory $SOURCE_DIR does not exist."
+    exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.txt" -mtime "$DAYS")
-echo Files:$FILES
+# Check if destination directory exists
+if [ ! -d "$DESTINATION_DIR" ]; then
+    echo "Destination directory $DESTINATION_DIR does not exist."
+    exit 1
+fi
 
-if [ ! -z "$FILES" ]  
-    then
-    echo files are found
-    ZIP_FILE=$DESTINATION_DIR/app-logs-$TIME_STAMP.zip
-    find $SOURCE_DIR -name "*.txt" -mtime "$DAYS"| zip $ZIP_FILE -@
+# Perform synchronization using rsync
+rsync -av --update "$SOURCE_DIR/" "$DESTINATION_DIR/"
 
-        if [ -f $ZIP_FILE ]
-        then 
-
-        echo succesfully zipped files older than $DAYS
-        while IFS= read -r file
-        do
-        echo "Deleting file:$file"
-        rm -rf $FILES
-        done <<< $FILES
-
-    else
-        echo -e "Failed  zipping files older than $DAYS"
-        exit 1
-        fi
+# Check if rsync command was successful
+if [ $? -eq 0 ]; then
+    echo "Synchronization complete."
 else
-  echo files are not found
+    echo "Error during synchronization."
+    exit 1
 fi
-
-
-
-#true only if files are epmty if files are not empty it gives false , then reslut will go to else
-#instead of this we can also use  -n or -f
