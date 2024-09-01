baf0b1a68cdf35df2433677673f29c17e1deb081
sujithyerra4 <sujithyerra483@gmail.com>
2024-09-01 11:40:12 +0530

    count

--- 01-count.sh
+++ 01-count.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=$1
+FILE=$1  #give path
 
 
 USAGE(){
@@ -14,7 +14,12 @@ fi
 
 
 # Remove special characters, convert to lowercase, replace spaces with newlines, and count words
- awk '{print}' $FILE | tr -c '[:alnum:]'  |tr -s '\n'|tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -rn|head -n 5 
+ awk '{print}' $FILE | tr -c '[:alnum:]' '[\n*]' |tr -s '\n'|tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -rn|head -n 5 
 
 
 
+# -c : selects all characters except alphanumeric characters (letters and digits).
+# \n* - all the selected from -c in to new line 
+# -s '\n' -will delete repeated new line  
+# uniq -c  Counts occurrences of each unique word.
+# sort -rn: Sorts the counted words in descending numerical order.
---
+++ 02-replace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILE=$1  #give path
+
+
+USAGE(){
+    echo "USAGE: sudo sh $0 <filename>"
+    exit 1
+}
+if [ $# -eq 0 ]
+then
+USAGE
+fi
+file=$(find $FILE -name "*.txt")
+while IFS = read -r fil
+do
+
+# awk '{print}'  $file|
+
+echo ${a//the/tehy}
+
+done <<< $FILE
