ba9531c87c03d5aee594704a25298b058268d8ab
sujithyerra4 <sujithyerra483@gmail.com>
2024-09-01 10:19:46 +0530

    count

---
+++ 01-count.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE=$1
+
+
+USAGE(){
+    echo "USAGE: sudo sh $0 $FILE"
+}
+if [ $# -eq 0 ]
+then
+USAGE
+fi
+
+while IFS= read -r file
+do 
+
+echo $file|tr '[:upper:]' '[:lower:]'|wc -l
+
+
+done <<< $FILE
